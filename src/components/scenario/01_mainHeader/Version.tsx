b3382f43d32f0754a56d9b8106140ab719a51d53
Prof Chaos <leperespikasse@gmail.com>
2024-07-18 14:13:49 +0200

    ...

--- src/components/scenario/01_mainHeader/Version.tsx
+++ src/components/scenario/01_mainHeader/Version.tsx
@@ -1,6 +1,6 @@
 // src/components/scenario/01_mainHeader/Version.tsx
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { ScnCompAttribute } from '@root/interfaces';
 import { ascii } from '@utils/dataFactories';
 import UseCommComponent from '@hooks/UseCommComponent';
@@ -10,13 +10,13 @@ const Version: React.FC<ScnCompAttribute> = ({ mySubscriber }) => {
   const [getValue2, setValue2] = useState<any>();
   const [getRawValue2, setRawValue2] = useState<any>();
 
-  const myHooks = {getValue2, setValue2, getRawValue2, setRawValue2}
-  UseCommComponent( {mySubscriber, myName:"version", myType:ascii(4), myHooks});
-  
-    return (
+  const myHooks = { getValue2, setValue2, getRawValue2, setRawValue2 }
+  UseCommComponent({ mySubscriber, myName: "version", myType: ascii(4), myHooks });
+
+  return (
     <>
-    <div>Coucou je suis la version ! <span>{getValue2}</span></div>
-    <div>Coucou je suis la version ! <span>{getRawValue2}</span></div>
+      <div>Coucou je suis la version ! <span>{getValue2}</span></div>
+      <div>Coucou je suis la version ! <span>{getRawValue2}</span></div>
     </>
   );
 };
